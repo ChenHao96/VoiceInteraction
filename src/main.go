994aeb7b64e27cec360198b950b0ed13f1817e8a
陈昊 <949727157@qq.com>
2016-08-19 21:02:18 +0800

    调整并测试代码

--- src/main.go
+++ src/main.go
@@ -8,6 +8,10 @@ import (
 	"os"
 	"os/exec"
 	"org/StevenChen/service"
+	"time"
+	"strings"
+	"syscall"
+	"path/filepath"
 )
 
 const (
@@ -20,38 +24,28 @@ const (
 */
 func main() {
 
-	resourcePath := os.Getenv("GOPATH") + "/resources/"
+	resourcePath, _ := filepath.Abs(filepath.Dir(os.Args[0]))
 
 	ttsUtil := tts.NewAPI_Util(API_Key, Secret_Key)
-	voiceUtil := yuyin.API_Util{Credentials:ttsUtil.Credentials,Cuid:ttsUtil.Cuid}
+	voiceUtil := yuyin.API_Util{Credentials:ttsUtil.Credentials, Cuid:ttsUtil.Cuid}
 
-	playFile := resourcePath + "test.mp3"
-	recordFile := resourcePath + "test.wav"
-
-	/*
-		打开linux下的录音软件
-		-r 采样率
-		-t 文件格式
-		-c 声道
-		-f 位深
-		-d 录音时间(单位；秒)
-		arecord的操作可以查看：http://blog.chinaunix.net/uid-29616823-id-4761787.html
-	*/
-	record := exec.Command("arecord", "-r", "16000", "-t", "wav", "-c", "1", "-f", "S16_LE", recordFile)
-	record.Stderr = os.Stderr
-	record.Stdout = os.Stdout
-
-	/*
-		linux 下安装下面的库
-		sudo apt-get install sox libsox-fmt-all
-	*/
-	play := exec.Command("play", playFile)
-	play.Stderr = os.Stderr
-	play.Stdout = os.Stdout
+	playFile := resourcePath + "\test.mp3"
+	recordFile := resourcePath + "\test.wav"
 
 	for {
+		/*
+			打开linux下的录音软件
+			-r 采样率
+			-t 文件格式
+			-c 声道
+			-f 位深
+			-d 录音时间(单位；秒)
+			arecord的操作可以查看：http://blog.chinaunix.net/uid-29616823-id-4761787.html
+		*/
+		record := exec.Command("arecord", "-r", "16000", "-t", "wav", "-c", "1", "-f", "S16_LE", recordFile)
 		go func() {
 			fmt.Println("开始聆听...")
+			time.Sleep(200 * time.Millisecond)
 			if err := record.Run(); nil != err {
 				fmt.Println(err)
 			}
@@ -59,16 +53,7 @@ func main() {
 
 		/**********************************************************/
 		/*********************此处还需要改进************************/
-		readLine := bufio.NewReader(os.Stdin)
-		for {
-			if key, err := readLine.ReadByte(); nil != err {
-				fmt.Println(err)
-			} else {
-				if byte('e') == key || byte('q') == key {
-					break
-				}
-			}
-		}
+		bufio.NewReader(os.Stdin).ReadByte()
 		/**********************************************************/
 		record.Process.Kill()
 		fmt.Println("结束聆听。")
@@ -78,16 +63,31 @@ func main() {
 
 		if len(result.Result) <= 0 {
 			fmt.Println("未识别到语音内容，识别无结果。")
+			time.Sleep(200 * time.Millisecond)
 			continue
 		}
 		fmt.Println("语音识别结果:", result.Result)
 
+		beExit := false
 		answer := service.DoSomething(result.Result)
+		if strings.Contains(answer, "闭嘴") {
+			answer = "虽然你这么说我会很伤心，但我还是要走的，再见。"
+		}
 		fmt.Println("响应结果(回答):", answer)
 
+		/*
+			linux下安装下面的库
+			sudo apt-get install sox libsox-fmt-all
+		*/
 		ttsUtil.Text2AudioFile(playFile, answer)
+		play := exec.Command("play", playFile)
 		if err := play.Run(); nil != err {
 			fmt.Println(err)
+		}
+
+		if beExit {
+			fmt.Println("正在退出...")
+			syscall.Exit(0)
 		}
 	}
 }
