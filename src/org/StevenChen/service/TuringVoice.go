9cd8789b5ba4bab1b99445c5c12f16a2a6776377
陈昊 <949727157@qq.com>
2016-08-20 15:12:25 +0800

    修改图灵回馈调用

--- src/org/StevenChen/service/TuringVoice.go
+++ src/org/StevenChen/service/TuringVoice.go
@@ -4,10 +4,12 @@ import "com/tuling123"
 
 const APT_KEY string = "d975f8141aa550cea27b7f48dd50c48d"
 
+var tuLingAPI = tuling123.NewAPI_Request(APT_KEY)
+
 func DoSomething(result []string) string {
 	/**
 		do something...
 		添加你自己的回答
 	 */
-	return tuling123.NewAPI_Request(APT_KEY).Talk(result[0]);
+	return tuLingAPI.Talk(result[0]);
 }
