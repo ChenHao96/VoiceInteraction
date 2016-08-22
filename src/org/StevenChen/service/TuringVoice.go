c8dea849f09be1085b340e7b02e0011f91e90862
陈昊 <949727157@qq.com>
2016-08-22 13:00:05 +0800

    将图灵机器人的API调用改为加密方式

--- src/org/StevenChen/service/TuringVoice.go
+++ src/org/StevenChen/service/TuringVoice.go
@@ -1,12 +1,15 @@
 package service
 
-import "com/tuling123"
-
-const (
-	API_KEY string = "89b298ba45ec4a479dd9f20076d82b81"
+import (
+	"com/tuling123/secret"
 )
 
-var tuLingAPI = tuling123.NewAPI_Request(API_KEY)
+const (
+	SECRET string = "4145a1cb5f92901b"
+	API_KEY string = "d975f8141aa550cea27b7f48dd50c48d"
+)
+
+var tuLingAPI = secret.NewAPI_Request(API_KEY, SECRET)
 
 func DoSomething(result []string) string {
 	/**
