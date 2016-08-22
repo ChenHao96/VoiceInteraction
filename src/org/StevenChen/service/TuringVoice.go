c45f09ddc6d990b869700c03d60fa224c26847fc
陈昊 <949727157@qq.com>
2016-08-22 16:34:45 +0800

    修改非加密方式请求

--- src/org/StevenChen/service/TuringVoice.go
+++ src/org/StevenChen/service/TuringVoice.go
@@ -1,15 +1,16 @@
 package service
 
 import (
-	"com/tuling123/secret"
+	"com/tuling123/normal"
 )
 
-const (
-	SECRET string = "4145a1cb5f92901b"
-	API_KEY string = "d975f8141aa550cea27b7f48dd50c48d"
-)
+//const (
+//	SECRET string = "4145a1cb5f92901b"
+//	API_KEY string = "d975f8141aa550cea27b7f48dd50c48d"
+//)
+//var tuLingAPI = secret.NewAPI_Request(API_KEY, SECRET)
 
-var tuLingAPI = secret.NewAPI_Request(API_KEY, SECRET)
+var tuLingAPI = normal.NewAPI_Request("89b298ba45ec4a479dd9f20076d82b81")
 
 func DoSomething(result []string) string {
 	/**
