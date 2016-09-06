5dfbfbc99ccb43332320940b08ffe1389275cc6b
陈昊 <949727157@qq.com>
2016-09-06 14:05:16 +0800

    添加测试代码

--- src/org/StevenChen/util/AES_Util.go
+++ src/org/StevenChen/util/AES_Util.go
@@ -4,7 +4,31 @@ import (
 	"bytes"
 	"crypto/cipher"
 	"crypto/aes"
+	"fmt"
+	"crypto/md5"
 )
+
+func main() {
+
+	secret := "37a0aa9075ec500d"
+	api_key := "89b298ba45ec4a479dd9f20076d82b81"
+	cmd := "你叫什么"
+	data := "{\"key\":\"" + api_key + "\",\"info\":\"" + cmd + "\"}"
+	timestamp := "1473140577124"
+
+	keyParam := secret + timestamp + api_key;
+	fmt.Println("keyParam:\n", keyParam)
+
+	key := md5.Sum([]byte(keyParam))
+	fmt.Println("MD5-Key:\n", fmt.Sprintf("%x", key))
+	key2 := make([]byte, len(key))
+	for index, value := range key {
+		key2[index] = value
+	}
+
+	data2, _ := AesEncrypt([]byte(data), key2)
+	fmt.Println("AES-MD5-data:\n", data2)
+}
 
 func AesEncrypt(origData, key []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
